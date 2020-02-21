6a887fa4e7377f4c10197db5c2cf5a7a5cba4da1
Henry <1309658793@qq.com>
2020-02-21 17:30:56 +0800

    删除无用代码

--- source/about/index.md
+++ source/about/index.md
@@ -2,8 +2,6 @@
 layout: page
 title: about
 body: [article, grid, comments]
-valine:
-  placeholder: 有什么想对我说的呢？
 sidebar: false
 ---
 你好
--- themes/material-x/source/js/plain.js
+++ themes/material-x/source/js/plain.js
@@ -1,4 +1,3 @@
 document.write('欢迎来自 ' + returnCitySN['cname'] + ' 的朋友')
 document.write('<br>您的 IP 是: ' + returnCitySN['cip'])
 document.write('<br>您使用的是: ' + platform.description)
-document.write('<br>今天的天气为: ' + weatherRes[0] + ', 温度为: ' + weatherRes[1] )
